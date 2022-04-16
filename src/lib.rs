efc5a21d0e4158da32ea312e0868b09f313b798a
Jason Francis <jafrancis999@gmail.com>
2022-04-15 22:52:37 -0400

    fix incorrectly placed attributes in readme of clone_block
    fixes #4

--- src/lib.rs
+++ src/lib.rs
@@ -64,23 +64,23 @@
 //!     // default panic:
 //!     //     move |#[weak(or_panic)] value| {}
 //!     //     move |#[weak(self or_panic)] this| {}
-//!     //     move #[default_panic] |#[weak(self)] this| {}
+//!     //     #[default_panic] move |#[weak(self)] this| {}
 //!     //
 //!     // default return:
 //!     //     move |#[weak(or_return)] value| {}
 //!     //     move |#[weak(or_return 123)] value| {}
 //!     //     move |#[weak(self or_return)] this| {}
 //!     //     move |#[weak(self or_return 123)] this| {}
-//!     //     move #[default_return] |#[weak(self)] this| {}
-//!     //     move #[default_return 123] |#[weak(self)] this| {}
+//!     //     #[default_return] move |#[weak(self)] this| {}
+//!     //     #[default_return 123] move |#[weak(self)] this| {}
 //!     //
 //!     // default alternative:
 //!     //     move |#[weak(or 123)] value| {}
 //!     //     move |#[weak(self.myvalue or 123)] value| {}
 //!     //
 //!     // forcing an Option when another default is present:
-//!     //     move #[default_panic] |#[weak(self)] this, #[weak(allow_none)] value| {}
-//!     //     move #[default_panic] |#[weak(self)] this, #[weak(self.myvalue allow_none)] value| {}
+//!     //     #[default_panic] move |#[weak(self)] this, #[weak(allow_none)] value| {}
+//!     //     #[default_panic] move |#[weak(self)] this, #[weak(self.myvalue allow_none)] value| {}
 //!
 //!     // equivalent to glib::closure!
 //!     let add = #[closure] |a: i32, b: i32| a + b;
