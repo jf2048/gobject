a852cc2d2914f1cc2a375c9033c9e9f0fae7aa9a
Jason Francis <jafrancis999@gmail.com>
2022-05-16 12:38:25 -0400

    add serde/variant funcs for gdk content serialization

--- serde/gdk4.rs
+++ serde/gdk4.rs
@@ -1,4 +1,5 @@
-use serde::{Deserialize, Deserializer, Serialize, Serializer};
+use serde::{de, Deserialize, Deserializer, Serialize, Serializer};
+use std::{borrow::Cow, marker::PhantomData};
 
 pub mod rectangle {
     use super::*;
@@ -28,4 +29,85 @@ pub mod rgba {
         Ok(gdk4::RGBA::new(r, g, b, a))
     }
     declare_optional!(gdk4::RGBA);
+}
+
+pub trait MimeType {
+    fn mime_type() -> Cow<'static, str>;
+}
+
+pub struct Content<T, M: MimeType>(PhantomData<T>, PhantomData<M>);
+
+impl<T, M: MimeType> Content<T, M> {
+    pub fn serialize<S: Serializer>(c: &T, s: S) -> Result<S::Ok, S::Error>
+    where
+        T: glib::ToValue,
+    {
+        use gdk4::gio::prelude::*;
+        let stream = gdk4::gio::MemoryOutputStream::new_resizable();
+        glib::MainContext::ref_thread_default()
+            .block_on(async {
+                gdk4::content_serialize_future(
+                    &stream,
+                    &*M::mime_type(),
+                    &c.to_value(),
+                    glib::PRIORITY_DEFAULT,
+                )
+                .await?;
+                stream.close_future(glib::PRIORITY_DEFAULT).await
+            })
+            .unwrap();
+        stream.steal_as_bytes().as_ref().serialize(s)
+    }
+    pub fn deserialize<'de, D: Deserializer<'de>>(d: D) -> Result<T, D::Error>
+    where
+        T: glib::value::ValueType,
+    {
+        use gdk4::gio::prelude::*;
+        let bytes = crate::glib::bytes::deserialize(d)?;
+        let stream = gdk4::gio::MemoryInputStream::from_bytes(&bytes);
+        glib::MainContext::ref_thread_default()
+            .block_on(async {
+                let value = gdk4::content_deserialize_future(
+                    &stream,
+                    &*M::mime_type(),
+                    T::Type::static_type(),
+                    glib::PRIORITY_DEFAULT,
+                )
+                .await?;
+                stream.close_future(glib::PRIORITY_DEFAULT).await?;
+                Ok::<glib::Value, glib::Error>(value)
+            })
+            .map_err(de::Error::custom)?
+            .get()
+            .map_err(de::Error::custom)
+    }
+}
+
+pub struct ContentOptional<T, M: MimeType>(PhantomData<T>, PhantomData<M>);
+
+impl<T, M: MimeType> ContentOptional<T, M> {
+    pub fn serialize<S: Serializer>(c: &Option<T>, s: S) -> Result<S::Ok, S::Error>
+    where
+        T: glib::ToValue,
+    {
+        #[derive(serde::Serialize)]
+        #[serde(transparent)]
+        struct Writer<'w, T: glib::ToValue, M: MimeType>(
+            #[serde(with = "Content::<T, M>")] &'w T,
+            #[serde(skip)] PhantomData<M>,
+        );
+        serde::Serialize::serialize(&c.as_ref().map(|c| Writer::<T, M>(c, PhantomData)), s)
+    }
+    pub fn deserialize<'de, D: Deserializer<'de>>(d: D) -> Result<Option<T>, D::Error>
+    where
+        T: glib::value::ValueType,
+    {
+        #[derive(serde::Deserialize)]
+        #[serde(transparent)]
+        struct Reader<T: glib::value::ValueType, M: MimeType>(
+            #[serde(with = "Content::<T, M>")] T,
+            #[serde(skip)] PhantomData<M>,
+        );
+        <Option<Reader<T, M>> as serde::Deserialize>::deserialize(d).map(|o| o.map(|o| o.0))
+    }
 }
--- src/variant/gdk4.rs
+++ src/variant/gdk4.rs
@@ -1,5 +1,6 @@
+use crate::variant::glib::bytes;
 use glib::{ToVariant, Variant, VariantTy};
-use std::borrow::Cow;
+use std::{borrow::Cow, marker::PhantomData};
 
 pub mod rectangle {
     use super::*;
@@ -35,4 +36,88 @@ pub mod rgba {
         Some(gdk4::RGBA::new(r as f32, g as f32, b as f32, a as f32))
     }
     declare_optional!(gdk4::RGBA);
+}
+
+pub trait MimeType {
+    fn mime_type() -> Cow<'static, str>;
+}
+
+pub struct Content<T, M: MimeType>(PhantomData<T>, PhantomData<M>);
+
+impl<T, M: MimeType> Content<T, M> {
+    pub fn static_variant_type() -> Cow<'static, VariantTy> {
+        bytes::static_variant_type()
+    }
+    pub fn to_variant(c: &T) -> Variant
+    where
+        T: glib::ToValue,
+    {
+        use gdk4::gio::prelude::*;
+        let stream = gdk4::gio::MemoryOutputStream::new_resizable();
+        glib::MainContext::ref_thread_default()
+            .block_on(async {
+                gdk4::content_serialize_future(
+                    &stream,
+                    &*M::mime_type(),
+                    &c.to_value(),
+                    glib::PRIORITY_DEFAULT,
+                )
+                .await?;
+                stream.close_future(glib::PRIORITY_DEFAULT).await
+            })
+            .unwrap();
+        stream.steal_as_bytes().to_variant()
+    }
+    pub fn from_variant(variant: &Variant) -> Option<T>
+    where
+        T: glib::value::ValueType,
+    {
+        use gdk4::gio::prelude::*;
+        let bytes = bytes::from_variant(variant)?;
+        let stream = gdk4::gio::MemoryInputStream::from_bytes(&bytes);
+        glib::MainContext::ref_thread_default()
+            .block_on(async {
+                let value = gdk4::content_deserialize_future(
+                    &stream,
+                    &*M::mime_type(),
+                    T::Type::static_type(),
+                    glib::PRIORITY_DEFAULT,
+                )
+                .await?;
+                stream.close_future(glib::PRIORITY_DEFAULT).await?;
+                Ok::<glib::Value, glib::Error>(value)
+            })
+            .ok()?
+            .get()
+            .ok()
+    }
+}
+
+pub struct ContentOptional<T, M: MimeType>(PhantomData<T>, PhantomData<M>);
+
+impl<T, M: MimeType> ContentOptional<T, M> {
+    pub fn static_variant_type() -> Cow<'static, VariantTy> {
+        Cow::Borrowed(unsafe { VariantTy::from_str_unchecked("may") })
+    }
+    pub fn to_variant(c: &Option<T>) -> Variant
+    where
+        T: glib::ToValue,
+    {
+        match c.as_ref() {
+            Some(value) => Variant::from_some(&Content::<T, M>::to_variant(value)),
+            None => Variant::from_none(&*Content::<T, M>::static_variant_type()),
+        }
+    }
+    pub fn from_variant(variant: &Variant) -> Option<Option<T>>
+    where
+        T: glib::value::ValueType,
+    {
+        if !variant.is_type(&*Self::static_variant_type()) {
+            return None;
+        }
+        match variant.as_maybe() {
+            Some(variant) => Some(Some(Content::<T, M>::from_variant(&variant)?)),
+            None => Some(None),
+        }
+    }
 }
